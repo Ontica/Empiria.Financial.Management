9d1f4d1212dad4742b973aaae1f3d82c1d257687
José Manuel Cota <jmcota@ontica.org>
2024-10-07 14:45:14 -0600

    Cashflow modules were moved from Empiria.Cashflow repository

---
+++ Cashflow/Core.Tests/Helpers/TestingConstants.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Cashflow System Core Tests                 Component : Test Helpers                            *
+*  Assembly : Empiria.Cashflow.Core.Tests.dll            Pattern   : Testing constants                       *
+*  Type     : TestingConstants                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides testing constants for Empiria Cashflow Management System Core module.                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Tests.Cashflow {
+
+  /// <summary>Provides testing constants for Empiria Cashflow Management System Core module.</summary>
+  static public class TestingConstants {
+
+  }  // class TestingConstants
+
+}  // namespace Empiria.Tests.Cashflow
