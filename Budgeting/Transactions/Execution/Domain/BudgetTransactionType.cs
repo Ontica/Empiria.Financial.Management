07881eedf3fa616f6d3c2747e1e70852c0021cd6
José Manuel Cota <jmcota@ontica.org>
2024-10-15 19:05:59 -0600

    Rfx BudgetTransactionType was converted to power type

--- Budgeting/Transactions/Execution/Domain/BudgetTransactionType.cs
+++ Budgeting/Transactions/Execution/Domain/BudgetTransactionType.cs
@@ -1,21 +1,33 @@
 ﻿/* Empiria Financial *****************************************************************************************
 *                                                                                                            *
 *  Module   : Budget Transactions                        Component : Domain Layer                            *
-*  Assembly : Empiria.Budgeting.Transactions.dll         Pattern   : Infomation Holder                       *
+*  Assembly : Empiria.Budgeting.Transactions.dll         Pattern   : Power type                              *
 *  Type     : BudgetTransactionType                      License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : Represents a budget transaction type.                                                          *
+*  Summary  : Power type that describes a budget transaction.                                                *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using Empiria.Ontology;
+
 namespace Empiria.Budgeting.Transactions {
 
-  /// <summary>Represents a budget transaction type.</summary>
-  internal class BudgetTransactionType : GeneralObject {
+  /// <summary>Power type that describes a budget transaction.</summary>
+  [Powertype(typeof(BudgetTransaction))]
+  public sealed class BudgetTransactionType : Powertype {
 
+    #region Constructors and parsers
 
-    static internal BudgetTransactionType Empty => BaseObject.ParseEmpty<BudgetTransactionType>();
+    private BudgetTransactionType() {
+      // Empiria powertype types always have this constructor.
+    }
 
-  }  // class BudgetTransactionType
+    static public new BudgetTransactionType Parse(int typeId) => Parse<BudgetTransactionType>(typeId);
+
+    static public new BudgetTransactionType Parse(string typeName) => Parse<BudgetTransactionType>(typeName);
+
+    #endregion Constructors and parsers
+
+  } // class BudgetTransactionType
 
 }  // namespace Empiria.Budgeting.Transactions
