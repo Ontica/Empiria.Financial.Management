424dba7148f93151d017aa9326614ab7bbd7aaf3
José Manuel Cota <jmcota@ontica.org>
2024-10-18 21:27:44 -0600

    Rfx SATProducto/SATUnidadMedida were moved from Empiria.Central

---
+++ Billing/Core/SATCatalogues/SATProducto.cs
@@ -0,0 +1,53 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Information Holder                      *
+*  Type     : SATProducto                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Producto o servicio de acuerdo al catálogo del SAT México para facturación electrónica.        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Billing.SATCatalogues {
+
+  /// <summary>Producto o servicio de acuerdo al catálogo del SAT México para facturación electrónica.</summary>
+  public class SATProducto : GeneralObject {
+
+    #region Constructors and parsers
+
+    static public SATProducto Parse(int id) => ParseId<SATProducto>(id);
+
+    static public SATProducto Parse(string uid) => ParseKey<SATProducto>(uid);
+
+    static public FixedList<SATProducto> GetList() {
+      return BaseObject.GetList<SATProducto>()
+                       .ToFixedList();
+    }
+
+    static public SATProducto Empty => ParseEmpty<SATProducto>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string Clave {
+      get {
+        return ExtendedDataField.Get<string>("clave");
+      }
+      private set {
+        ExtendedDataField.Set("clave", value);
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(Name, Clave);
+      }
+    }
+
+    #endregion Properties
+
+  } // class SATProducto
+
+} // namespace Empiria.Billing.SATCatalogues
---
+++ Billing/Core/SATCatalogues/SATUnidadMedida.cs
@@ -0,0 +1,53 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Information Holder                      *
+*  Type     : SATUnidadMedida                            License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unidad de medida de acuerdo al catálogo del SAT México para facturación electrónica.           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Billing.SATCatalogues {
+
+  /// <summary>Unidad de medida de acuerdo al catálogo del SAT México para facturación electrónica.</summary>
+  public class SATUnidadMedida : GeneralObject {
+
+    #region Constructors and parsers
+
+    static public SATUnidadMedida Parse(int id) => ParseId<SATUnidadMedida>(id);
+
+    static public SATUnidadMedida Parse(string uid) => ParseKey<SATUnidadMedida>(uid);
+
+    static public FixedList<SATUnidadMedida> GetList() {
+      return BaseObject.GetList<SATUnidadMedida>()
+                       .ToFixedList();
+    }
+
+    static public SATUnidadMedida Empty => ParseEmpty<SATUnidadMedida>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public string Clave {
+      get {
+        return ExtendedDataField.Get<string>("clave");
+      }
+      private set {
+        ExtendedDataField.Set("clave", value);
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(Name, Clave);
+      }
+    }
+
+    #endregion Properties
+
+  } // class SATUnidadMedida
+
+} // namespace Empiria.Billing.SATCatalogues
