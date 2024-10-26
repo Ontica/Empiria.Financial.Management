28f3025a97356d4fa6ff7d6b5e9b27a473a0954a
José Manuel Cota <jmcota@ontica.org>
2024-10-26 12:55:31 -0600

    BillType and BillCategory types were added

---
+++ Billing/Core/Domain/BillCategory.cs
@@ -0,0 +1,110 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Category Type                           *
+*  Type     : BillCategory                               License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a bill category like product sales or purchase, employee paycheck, etc.             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.StateEnums;
+
+namespace Empiria.Billing {
+
+  /// <summary>Represents a bill category like product sales or purchase, employee paycheck, etc.</summary>
+  public class BillCategory : GeneralObject {
+
+    #region Constructors and parsers
+
+    private BillCategory() {
+      // Required by Empiria Framework
+    }
+
+    internal BillCategory(BillType billType, string name) {
+      Assertion.Require(billType, nameof(billType));
+
+      name = EmpiriaString.Clean(name);
+
+      Assertion.Require(name, nameof(name));
+
+      BillType = billType;
+      Name = name;
+    }
+
+    static public BillCategory Parse(int id) => ParseId<BillCategory>(id);
+
+    static public BillCategory Parse(string uid) => ParseKey<BillCategory>(uid);
+
+    static public FixedList<BillCategory> GetList() {
+      return BaseObject.GetList<BillCategory>()
+                       .ToFixedList();
+    }
+
+    static public BillCategory Empty => ParseEmpty<BillCategory>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public BillType BillType {
+      get {
+        return base.ExtendedDataField.Get("billTypeId", BillType.Empty);
+      }
+      private set {
+        base.ExtendedDataField.SetIf("billTypeId", value.Id, value.Id != -1);
+      }
+    }
+
+
+    public string Description {
+      get {
+        return base.ExtendedDataField.Get("description", string.Empty);
+      }
+      private set {
+        base.ExtendedDataField.SetIfValue("description", EmpiriaString.TrimAll(value));
+      }
+    }
+
+
+    public bool IsAssignable {
+      get {
+        return base.ExtendedDataField.Get("isAssignable", IsEmptyInstance ? false : true);
+      }
+      private set {
+        base.ExtendedDataField.SetIf("isAssignable", value, value == false);
+      }
+    }
+
+
+    public BillCategory Parent {
+      get {
+        return base.ExtendedDataField.Get("parentCategoryId", BillCategory.Empty);
+      }
+      private set {
+        base.ExtendedDataField.SetIf("parentCategoryId", value.Id, value.Id != -1);
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        if (IsEmptyInstance) {
+        }
+        return EmpiriaString.BuildKeywords(Name, BillType.DisplayName, Parent.Name);
+      }
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    internal void Delete() {
+      base.Status = EntityStatus.Deleted;
+    }
+
+    #endregion Methods
+
+  }  // class BillCategory
+
+}  // namespace Empiria.Billing
---
+++ Billing/Core/Domain/BillType.cs
@@ -0,0 +1,37 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Power type                              *
+*  Type     : BillType                                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Power type that represents a bill type like a sales bill type or purchase order bill type,     *
+*             a credit note, a paycheck, a payment reception, etc.                                           *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Ontology;
+
+namespace Empiria.Billing {
+
+  /// <summary>Power type that represents a bill type like a sales bill type or purchase order bill type,
+  ///a credit note, a paycheck, a payment reception, etc.</summary>
+  [Powertype(typeof(Bill))]
+  public sealed class BillType : Powertype {
+
+    #region Constructors and parsers
+
+    private BillType() {
+      // Empiria powertype types always have this constructor.
+    }
+
+    static public new BillType Parse(int typeId) => Parse<BillType>(typeId);
+
+    static public new BillType Parse(string typeName) => Parse<BillType>(typeName);
+
+    static public BillType Empty => Parse("ObjectTypeInfo.BillType");
+
+    #endregion Constructors and parsers
+
+  }  // class BillType
+
+}  // namespace Empiria.Billing
