508dfdef255905dca6129be8eb45121c90c46909
José Manuel Cota <jmcota@ontica.org>
2024-10-25 20:52:01 -0600

    BillQuery and BillStatus types added

---
+++ Billing/Core/Domain/BillStatus.cs
@@ -0,0 +1,30 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Billing                                    Component : Domain Layer                            *
+*  Assembly : Empiria.Billing.Core.dll                   Pattern   : Enumeration                             *
+*  Type     : BillStatus                                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Enumerates the control status of a bill.                                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Billing {
+
+  /// <summary>Enumerates the control status of a bill.</summary>
+  public enum BillStatus {
+
+    Pending = 'P',
+
+    Validated = 'V',
+
+    Payed = 'Y',
+
+    Canceled = 'C',
+
+    Deleted = 'X',
+
+    All = '*',
+
+  }  // enum BillStatus
+
+}  // namespace Empiria.Billing
