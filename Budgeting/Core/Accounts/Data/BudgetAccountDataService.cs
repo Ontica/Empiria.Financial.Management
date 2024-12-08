34d0dd587e1a7b3a3bc72defb256477644100e9c
José Manuel Cota <jmcota@ontica.org>
2024-12-07 22:54:45 -0600

    BudgetAccountSearcher initial version added

---
+++ Budgeting/Core/Accounts/Data/BudgetAccountDataService.cs
@@ -0,0 +1,36 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Accounts                            Component : Data Layer                              *
+*  Assembly : Empiria.Budgeting.Core.dll                 Pattern   : Data Service                            *
+*  Type     : BudgetAccountDataService                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides data access services for budget accounts.                                             *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Data;
+
+namespace Empiria.Budgeting.Data {
+
+  /// <summary>Provides data access services for budget accounts.</summary>
+  static internal class BudgetAccountDataService {
+
+    static internal FixedList<BudgetAccount> SearchBudgetAcccounts(string filter, string sort) {
+
+      var sql = "SELECT * FROM FMS_BUDGET_ACCOUNTS";
+
+      if (!string.IsNullOrWhiteSpace(filter)) {
+        sql += $" WHERE {filter}";
+      }
+      if (!string.IsNullOrWhiteSpace(sort)) {
+        sql += $" ORDER BY {sort}";
+      }
+
+      var op = DataOperation.Parse(sql);
+
+      return DataReader.GetFixedList<BudgetAccount>(op);
+    }
+
+  }  // class BudgetAccountDataService
+
+}  // namespace Empiria.Budgeting.Data
---
+++ Budgeting/Core/Accounts/Domain/BudgetAccountSearcher.cs
@@ -0,0 +1,64 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Accounts                            Component : Domain Layer                            *
+*  Assembly : Empiria.Budgeting.Core.dll                 Pattern   : Service provider                        *
+*  Type     : BudgetAccountSearcher                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Searches budget accounts.                                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Parties;
+
+using Empiria.Budgeting.Data;
+
+namespace Empiria.Budgeting {
+
+  /// <summary>Searches budget accounts.</summary>
+  public class BudgetAccountSearcher {
+
+    private readonly BudgetType _budgetType;
+
+    public BudgetAccountSearcher(BudgetType budgetType) {
+
+      _budgetType = budgetType;
+    }
+
+    public FixedList<BudgetAccount> Search(Party party,
+                                           FixedList<BudgetAccountSegment> primarySegments) {
+
+      string primarySegmentsFilter = GetSegmentsFilter("BDG_ACCT_SEGMENT_02_ID", primarySegments);
+      string partyFilter = GetPartyFilter(party);
+      string budgetTypeFilter = GetBudgetTypeFilter();
+
+      var filter = new Filter(primarySegmentsFilter);
+
+      filter.AppendAnd(partyFilter);
+      filter.AppendAnd(budgetTypeFilter);
+
+      return BudgetAccountDataService.SearchBudgetAcccounts(filter.ToString(), "BDG_ACCT_CODE");
+    }
+
+
+    #region Helpers
+
+    private string GetBudgetTypeFilter() {
+      return $"BDG_ACCT_BUDGET_TYPE_ID = {_budgetType.Id}";
+    }
+
+
+    private string GetPartyFilter(Party party) {
+      return $"BDG_ACCT_SEGMENT_01_ID = {party.Id}";
+    }
+
+
+    private string GetSegmentsFilter(string segmentColumn,
+                                     FixedList<BudgetAccountSegment> segments) {
+      return SearchExpression.ParseInSet(segmentColumn, segments.Select(x => x.Id));
+    }
+
+    #endregion Helpers
+
+  }  // public class BudgetAccountSearcher
+
+}  // namespace Empiria.Budgeting
