3b82c95a470522f4f7b2b4f7fcb44ef34fe3fca0
José Manuel Cota <jmcota@ontica.org>
2024-12-07 22:52:49 -0600

    BudgetAccountSegmentLink type added inherited from BaseObjectLink

---
+++ Budgeting/Core/Accounts/Domain/BudgetAccountSegmentLink.cs
@@ -0,0 +1,81 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Accounts                            Component : Domain Layer                            *
+*  Assembly : Empiria.Budgeting.Core.dll                 Pattern   : Link Type                               *
+*  Type     : BudgetAccountSegmentLink                   License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Links a BudgetAccountSegment with other objects, like projects, products or geolocations.      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Products;
+using Empiria.Projects;
+
+namespace Empiria.Budgeting {
+
+  /// <summary>Links a BudgetAccountSegment with other objects, like projects,
+  /// products or geolocations.</summary>
+  public class BudgetAccountSegmentLink : BaseObjectLink {
+
+    #region Types
+
+    static public BaseObjectLinkType ProductLink =>
+                    BaseObjectLinkType.Parse("ObjectTypeInfo.BudgetAccountSegmentLink.Product");
+
+    static public BaseObjectLinkType ProjectLink =>
+                    BaseObjectLinkType.Parse("ObjectTypeInfo.BudgetAccountSegmentLink.Project");
+
+    #endregion Types
+
+    #region Constructors and parsers
+
+    protected BudgetAccountSegmentLink(BaseObjectLinkType powertype) : base(powertype) {
+      // Required by Empiria Framework for all partitioned types.
+    }
+
+    internal BudgetAccountSegmentLink(BudgetAccountSegment segment, Product product)
+              : base(ProductLink, segment, product) {
+      // no-op
+    }
+
+
+    internal BudgetAccountSegmentLink(BudgetAccountSegment segment, Project project)
+                : base(ProjectLink, segment, project) {
+      // no-op
+    }
+
+    static public BudgetAccountSegmentLink Parse(int id) => ParseId<BudgetAccountSegmentLink>(id);
+
+    static public BudgetAccountSegmentLink Parse(string uid) => ParseKey<BudgetAccountSegmentLink>(uid);
+
+
+    static public FixedList<BudgetAccountSegment> GetBudgetAccountSegmentsForProduct(Product product) {
+      Assertion.Require(product, nameof(product));
+
+      return GetBaseObjectsFor<BudgetAccountSegment>(ProductLink, product);
+    }
+
+    static public BudgetAccountSegmentLink Empty => ParseEmpty<BudgetAccountSegmentLink>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    internal BudgetAccountSegment BudgetAccountSegment {
+      get {
+        return base.GetBaseObject<BudgetAccountSegment>();
+      }
+    }
+
+
+    public override string Keywords {
+      get {
+        return EmpiriaString.BuildKeywords(this.BudgetAccountSegment.Keywords);
+      }
+    }
+
+    #endregion Properties
+
+  } // class BudgetAccountSegmentLink
+
+} // namespace Empiria.Budgeting
