4931be14017b57658c4fff54ee4c7a5479bf0e0e
José Manuel Cota <jmcota@ontica.org>
2025-04-30 10:38:32 -0400

    Financial Management Reporting core and tests modules added

---
+++ Reporting/Core.Tests/Empiria.Financial.Reporting.Core.Tests.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/*Empiria Financial ******************************************************************************************
+*                                                                                                            *
+*  System   : Financial Management                         Module  : Reporting Services Tests                *
+*  Assembly : Empiria.Financial.Reporting.Tests.dll        Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Tests suite for Empiria Financial Management Reporting services.                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
