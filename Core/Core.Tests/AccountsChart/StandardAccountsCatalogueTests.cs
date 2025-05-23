71e5e86866ffd6bb55c4f694f770841050ee1c64
José Manuel Cota <jmcota@ontica.org>
2025-05-23 19:26:01 -0400

    StandardAccountsCatalogue type added

---
+++ Core/Core.Tests/AccountsChart/StandardAccountsCatalogueTests.cs
@@ -0,0 +1,44 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Accounts Management                        Component : Test cases                              *
+*  Assembly : Empiria.Tests.Financial.Accounts.dll       Pattern   : Unit tests                              *
+*  Type     : StandardAccountsCatalogueTests             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for StandardAccountsCatalogue type.                                                 *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Financial;
+
+namespace Empiria.Tests.Financial.Accounts {
+
+  /// <summary>Unit tests for StandardAccountsCatalogue type.</summary>
+  public class StandardAccountsCatalogueTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Read_All_Standard_Accounts_Catalogues() {
+      var sut = BaseObject.GetFullList<StandardAccountsCatalogue>();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Read_Empty_StandardAccountsCatalogue() {
+      var sut = StandardAccountsCatalogue.Empty;
+
+      Assert.NotNull(sut);
+      Assert.Equal(StandardAccountsCatalogue.Parse("Empty"), sut);
+      Assert.Equal(-1, sut .Id);
+    }
+
+    #endregion Facts
+
+  }  // class StandardAccountsCatalogueTests
+
+}  // namespace Empiria.Tests.Financial.Accounts
---
+++ Core/Core/AccountsChart/Domain/StandardAccountsCatalogue.cs
@@ -0,0 +1,37 @@
+﻿/* Empiria Financial  ******************************************************************************************
+*                                                                                                            *
+*  Module   : AccountsChart                              Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : StandardAccountsCatalogue                  License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Defines an standard accounts catalogue.                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial {
+
+  /// <summary>Defines an standard accounts catalogue.</summary>
+  public class StandardAccountsCatalogue : CommonStorage {
+
+    #region Constructors and parsers
+
+    protected StandardAccountsCatalogue() {
+      // Required by Empiria Framework
+    }
+
+    static public StandardAccountsCatalogue Parse(int id) => ParseId<StandardAccountsCatalogue>(id);
+
+    static public StandardAccountsCatalogue Parse(string uid) => ParseKey<StandardAccountsCatalogue>(uid);
+
+    static public StandardAccountsCatalogue Empty => ParseEmpty<StandardAccountsCatalogue>();
+
+    static public FixedList<StandardAccountsCatalogue> GetList() {
+      return GetList<StandardAccountsCatalogue>()
+            .ToFixedList();
+    }
+
+    #endregion Constructors and parsers
+
+  } // class StandardAccountsCatalogue
+
+} // namespace Empiria.Financial
