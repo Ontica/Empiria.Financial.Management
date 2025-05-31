ef37aa464fd349db63f4ddbd024bcf16edde5f3a
José Manuel Cota <jmcota@ontica.org>
2025-05-30 23:17:17 -0400

    Rfx CreditAttributes and CreditFinancialData now implement abstracts

---
+++ Core/Core.Tests/AccountsChart/CreditAcccountTests.cs
@@ -0,0 +1,47 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Accounts Management                        Component : Test cases                              *
+*  Assembly : Empiria.Tests.Financial.Accounts.dll       Pattern   : Unit tests                              *
+*  Type     : CreditAcccountTests                        License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for credit accounts objects.                                                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Financial;
+
+namespace Empiria.Tests.Financial.Accounts {
+
+  /// <summary>Unit tests for credit accounts objects.</summary>
+  public class CreditAcccountTests {
+
+    #region Facts
+    [Fact]
+    public void Should_Update_CreditData() {
+      var attributes = new CreditAttributes {
+        NoCredito = "002000",
+        EtapaCredito = 1,
+        Acreditado = "La Vía Óntica SC",
+        TipoCredito = "Linea directa",
+      };
+
+      var fields = new FinancialAccountFields {
+        Attributes = attributes,
+      };
+
+      FinancialAccount sut = TestsObjects.TryGetObject<FinancialAccount>();
+
+      sut.Update(fields);
+
+      Assert.Equal(((CreditAttributes) fields.Attributes).Acreditado, ((CreditAttributes) sut.Attributes).Acreditado);
+      Assert.Equal(((CreditAttributes) fields.Attributes).EtapaCredito, ((CreditAttributes) sut.Attributes).EtapaCredito);
+    }
+
+
+    #endregion Facts
+
+  }  // class CreditAcccountTests
+
+}  // namespace Empiria.Tests.Financial.Accounts
---
+++ Core/Core/AccountsChart/Domain/CreditAttributes.cs
@@ -0,0 +1,88 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : FinancialAccounts                          Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : CreditAttributes                           License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Holds credit financial account attributes.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Json;
+
+namespace Empiria.Financial {
+
+  /// <summary>Holds credit financial account attributes.</summary>
+  public class CreditAttributes : AccountAttributes {
+
+    #region Constructors and Parsers
+
+    private readonly JsonObject _attributes = new JsonObject();
+
+    public CreditAttributes() {
+
+    }
+
+    internal CreditAttributes(JsonObject attributes) {
+      Assertion.Require(attributes, nameof(attributes));
+
+      _attributes = attributes;
+    }
+
+    #endregion Properties
+
+    #region Properties
+
+    public string NoCredito {
+      get {
+        return _attributes.Get("noCredito", string.Empty);
+      }
+      set {
+        _attributes.SetIfValue("noCredito", value);
+      }
+    }
+
+
+    public string Acreditado {
+      get {
+        return _attributes.Get("acreditado", string.Empty);
+      }
+      set {
+        _attributes.SetIfValue("acreditado", value);
+      }
+    }
+
+
+    public string TipoCredito {
+      get {
+        return _attributes.Get("tipoCredito", string.Empty);
+      }
+      set {
+        _attributes.SetIfValue("tipoCredito", value);
+      }
+    }
+
+
+    public int EtapaCredito {
+      get {
+        return _attributes.Get("etapaCredito", 0);
+      }
+      set {
+        _attributes.SetIfValue("etapaCredito", value);
+      }
+    }
+
+
+    #endregion Properties
+
+    #region Helpers
+
+    internal override string ToJsonString() {
+      return _attributes.ToString();
+    }
+
+    #endregion Helpers
+
+  } // class CreditAttributes
+
+} // namespace Empiria.Financial
