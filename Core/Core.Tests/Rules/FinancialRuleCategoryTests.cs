c1b4999664faa3e2fae619ae8797fd4f7703e8fb
José Manuel Cota <jmcota@ontica.org>
2025-08-03 12:25:39 -0400

    FinancialRuleCategory type added

---
+++ Core/Core.Tests/Rules/FinancialRuleCategoryTests.cs
@@ -0,0 +1,54 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Rules                            Component : Test cases                              *
+*  Assembly : Empiria.Financial.Core.Tests.dll           Pattern   : Unit tests                              *
+*  Type     : FinancialRuleCategoryTests                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Unit tests for inancialRuleCategory type.                                                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Xunit;
+
+using Empiria.Financial.Rules;
+
+namespace Empiria.Tests.Financial.Accounts {
+
+  /// <summary>Unit tests for inancialRuleCategory type.</summary>
+  public class FinancialRuleCategoryTests {
+
+    #region Facts
+
+    [Fact]
+    public void Should_Parse_All_Financial_Rules_Categories() {
+      var categories = FinancialRuleCategory.GetList();
+
+      foreach (var sut in categories) {
+        Assert.NotEmpty(sut.NamedKey);
+      }
+    }
+
+
+    [Fact]
+    public void Should_Read_All_Financial_Rules_Categories() {
+      var sut = FinancialRuleCategory.GetList();
+
+      Assert.NotNull(sut);
+      Assert.NotEmpty(sut);
+    }
+
+
+    [Fact]
+    public void Should_Read_Empty_FinancialRuleCategory() {
+      var sut = FinancialRuleCategory.Empty;
+
+      Assert.NotNull(sut);
+      Assert.Equal("Empty", sut.UID);
+      Assert.Equal(FinancialRuleCategory.Parse("Empty"), sut);
+    }
+
+    #endregion Facts
+
+  }  // class FinancialRuleCategoryTests
+
+}  // namespace Empiria.Tests.Financial.Accounts
---
+++ Core/Core/Rules/Domain/FinancialRuleCategory.cs
@@ -0,0 +1,42 @@
+﻿/* Empiria Financial  ****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Rules                            Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : FinancialRuleCategory                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a financial rule category.                                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial.Rules {
+
+  /// <summary>Represents a financial rule category.</summary>
+  public class FinancialRuleCategory : CommonStorage {
+
+    #region Constructors and parsers
+
+    protected FinancialRuleCategory() {
+      // Required by Empiria Framework
+    }
+
+    static public FinancialRuleCategory Parse(int id) => ParseId<FinancialRuleCategory>(id);
+
+    static public FinancialRuleCategory Parse(string uid) => ParseKey<FinancialRuleCategory>(uid);
+
+    static public FinancialRuleCategory Empty => ParseEmpty<FinancialRuleCategory>();
+
+    static public FixedList<FinancialRuleCategory> GetList() {
+      return GetStorageObjects<FinancialRuleCategory>();
+    }
+
+    #endregion Constructors and parsers
+
+    public new string NamedKey {
+      get {
+        return base.NamedKey;
+      }
+    }
+
+  } // class FinancialRuleCategory
+
+} // namespace Empiria.Financial.Rules
