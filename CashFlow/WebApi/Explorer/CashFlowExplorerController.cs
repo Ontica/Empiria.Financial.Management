1e0c7b1e1ce436337acf381751bc39af61581079
José Manuel Cota <jmcota@ontica.org>
2025-09-02 11:06:03 -0400

    ExportCashFlowToExcel web api and report builder added

--- CashFlow/WebApi/Explorer/CashFlowExplorerController.cs
+++ CashFlow/WebApi/Explorer/CashFlowExplorerController.cs
@@ -4,34 +4,54 @@
 *  Assembly : Empiria.CashFlow.WebApi.dll                  Pattern   : Query Web Api Controller              *
 *  Type     : CashFlowExplorerController                   License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary  : Web API used to retrive cash flow explorer information.                                        *
+*  Summary  : Query web API used to retrive cash flow explorer information.                                  *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
 using System.Threading.Tasks;
 using System.Web.Http;
 
+using Empiria.Storage;
 using Empiria.WebApi;
 
 using Empiria.CashFlow.Explorer.Adapters;
 using Empiria.CashFlow.Explorer.UseCases;
 
+using Empiria.CashFlow.Reporting;
+
 namespace Empiria.CashFlow.Explorer.WebApi {
 
-  /// <summary>Web API used to retrive and update cash ledger transactions.</summary>
+  /// <summary>Query web API used to retrive cash flow explorer information.</summary>
   public class CashFlowExplorerController : WebApiController {
 
     #region Web apis
-
 
     [HttpPost]
     [Route("v1/cash-flow/explorer")]
     public async Task<SingleObjectModel> ExploreCashFlow([FromBody] CashFlowExplorerQuery query) {
 
       using (var usecases = CashFlowExplorerUseCases.UseCaseInteractor()) {
-        CashFlowExplorerResultDto result = await usecases.ExploreCashFlow(query);
+        CashFlowExplorerResultDto explorerResult = await usecases.ExploreCashFlow(query);
 
-        return new SingleObjectModel(base.Request, result);
+        return new SingleObjectModel(base.Request, explorerResult);
+      }
+    }
+
+
+    [HttpPost]
+    [Route("v1/cash-flow/export")]
+    public async Task<SingleObjectModel> ExportCashFlowToExcel([FromBody] CashFlowExplorerQuery query) {
+
+      using (var usecases = CashFlowExplorerUseCases.UseCaseInteractor()) {
+        CashFlowExplorerResultDto explorerResult = await usecases.ExploreCashFlow(query);
+
+        var reportingService = CashFlowReportingService.ServiceInteractor();
+
+        FileDto report = reportingService.ExportCashFlowExplorerToExcel(explorerResult);
+
+        base.SetOperation($"Se exportó el reporte de flujo de efectivo a Excel.");
+
+        return new SingleObjectModel(base.Request, report);
       }
     }
 
---
+++ Reporting/Core/Builders/CashFlow/CashFlowExplorerToExcelBuilder.cs
@@ -0,0 +1,82 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Cash Flow Management                          Component : Reporting Services                   *
+*  Assembly : Empiria.Financial.Reporting.Core.dll          Pattern   : Report builder                       *
+*  Type     : CashFlowExplorerToExcelBuilder                License   : Please read LICENSE.txt file         *
+*                                                                                                            *
+*  Summary  : Builds an Excel file with cash flow explorer results.                                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+using Empiria.Office;
+using Empiria.Storage;
+
+using Empiria.CashFlow.Explorer.Adapters;
+
+namespace Empiria.CashFlow.Reporting {
+
+  /// <summary>Builds an Excel file with cash flow explorer results.</summary>
+  internal class CashFlowExplorerToExcelBuilder {
+
+    private readonly FileTemplateConfig _templateConfig;
+
+    private ExcelFile _excelFile;
+
+    public CashFlowExplorerToExcelBuilder(FileTemplateConfig templateConfig) {
+      Assertion.Require(templateConfig, nameof(templateConfig));
+
+      _templateConfig = templateConfig;
+    }
+
+
+    internal ExcelFile CreateExcelFile(CashFlowExplorerResultDto explorerResult) {
+      Assertion.Require(explorerResult, nameof(explorerResult));
+
+      _excelFile = new ExcelFile(_templateConfig);
+
+      _excelFile.Open();
+
+      SetHeader();
+
+      FillOut(explorerResult);
+
+      _excelFile.Save();
+
+      _excelFile.Close();
+
+      return _excelFile;
+    }
+
+
+    private void SetHeader() {
+      _excelFile.SetCell(_templateConfig.TitleCell, _templateConfig.Title);
+      _excelFile.SetCell(_templateConfig.CurrentTimeCell,
+                         $"Generado el: {DateTime.Now.ToString("dd/MMM/yyyy HH:mm")}");
+    }
+
+
+    private void FillOut(CashFlowExplorerResultDto explorerResult) {
+      int i = _templateConfig.FirstRowIndex;
+
+      foreach (var entry in explorerResult.Entries) {
+
+        _excelFile.SetCell($"A{i}", entry.CashAccountNo);
+        _excelFile.SetCell($"B{i}", entry.ConceptDescription);
+        _excelFile.SetCell($"C{i}", entry.Program);
+        _excelFile.SetCell($"D{i}", entry.Subprogram);
+        _excelFile.SetCell($"E{i}", entry.FinancingSource);
+        _excelFile.SetCell($"F{i}", entry.OperationType);
+        _excelFile.SetCell($"G{i}", entry.CurrencyCode);
+        _excelFile.SetCell($"H{i}", entry.Inflows);
+        _excelFile.SetCell($"I{i}", entry.Outflows);
+
+        i++;
+
+      }  //  foreach entry
+    }
+
+  } // class CashFlowExplorerToExcelBuilder
+
+} // namespace Empiria.CashFlow.Reporting
