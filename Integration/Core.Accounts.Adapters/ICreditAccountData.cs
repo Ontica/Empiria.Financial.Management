393617896c25e9def9ed92935ed35d789cb01490
José Manuel Cota <jmcota@ontica.org>
2025-09-03 22:04:17 -0400

    Integration.ExternalCreditSystemServices provider added

---
+++ Integration/Core.Accounts.Adapters/ICreditAccountData.cs
@@ -0,0 +1,34 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Integration services                       Component : Adapters Layer                          *
+*  Assembly : Empiria.Financial.Integration.dll          Pattern   : Adaptation Interface                    *
+*  Type     : ICreditAccountData                         License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Interface with credit account data used to connect with external systems.                      *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial.Adapters {
+
+  /// <summary>Interface with credit account data used to connect with external systems.</summary>
+  public interface ICreditAccountData {
+
+    string AccountNo {
+      get;
+    }
+
+    string CustomerName {
+      get;
+    }
+
+    string CustomerNo {
+      get;
+    }
+
+    string SubledgerAccountNo {
+      get;
+    }
+
+  }  // interface ICreditAccountData
+
+} // namespace Empiria.Financial.Adapters
---
+++ Integration/Core.Accounts.Adapters/ICreditAccountService.cs
@@ -0,0 +1,20 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Integration services                       Component : Adapters Layer                          *
+*  Assembly : Empiria.Financial.Integration.dll          Pattern   : Adaptation Interface                    *
+*  Type     : ICreditAccountService                      License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Interface used to retrieve credit accounts data from external systems.                         *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Financial.Adapters {
+
+  /// <summary>Interface used to retrieve credit accounts data from external systems.</summary>
+  public interface ICreditAccountService {
+
+    ICreditAccountData TryGetCredit(string creditNo);
+
+  }  // ICreditAccountService
+
+}  // namespace Empiria.Financial.Adapters
