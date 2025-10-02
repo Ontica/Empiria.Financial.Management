c8801d953c61648f7444692e004b70f3aa642644
José Manuel Cota <jmcota@ontica.org>
2025-10-02 09:09:26 -0400

    CashFlowSearchServices.SearchExternalCreditEntries service + web api added

---
+++ CashFlow/Explorer/UseCases/CashFlowSearchServices.cs
@@ -0,0 +1,59 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : CashFlow Explorer                          Component : Use cases Layer                         *
+*  Assembly : Empiria.CashFlow.Explorer.dll              Pattern   : Service provider                        *
+*  Type     : CashFlowSearchServices                     License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides services used to retrieve cash flow related data and entities.                        *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.DynamicData;
+using Empiria.Services;
+
+using Empiria.Financial;
+using Empiria.Financial.Adapters;
+
+namespace Empiria.CashFlow.Explorer.UseCases {
+
+  /// <summary>Provides services used to retrieve cash flow related data and entities.</summary>
+  public class CashFlowSearchServices : Service {
+
+    #region Constructors and parsers
+
+    protected CashFlowSearchServices() {
+      // no-op
+    }
+
+    static public CashFlowSearchServices ServiceInteractor() {
+      return CreateInstance<CashFlowSearchServices>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Services
+
+    public DynamicDto<ICreditEntryData> SearchExternalCreditEntries(RecordsSearchQuery query) {
+
+      ExternalCreditSystemServices provider = new ExternalCreditSystemServices();
+
+      FixedList<ICreditEntryData> entries = provider.GetCreditEntries(query.Keywords.ToFixedList(),
+                                                                        query.FromDate,
+                                                                        query.ToDate);
+      var columns = new DataTableColumn[6] {
+        new DataTableColumn("accountNo", "No crédito", "text"),
+        new DataTableColumn("subledgerAccountNo", "Auxiliar", "text"),
+        new DataTableColumn("applicationDate", "Fecha", "date"),
+        new DataTableColumn("operationTypeNo", "Clave", "text"),
+        new DataTableColumn("operationName", "Concepto", "text"),
+        new DataTableColumn("amount", "Importe", "decimal"),
+      }.ToFixedList();
+
+      return new DynamicDto<ICreditEntryData>(query, columns, entries);
+    }
+
+    #endregion Services
+
+  }  // class Services
+
+}  // namespace Empiria.CashFlow.Explorer.UseCases
--- CashFlow/WebApi/Explorer/SearchServicesController.cs
+++ CashFlow/WebApi/Explorer/SearchServicesController.cs
@@ -2,15 +2,16 @@
 *                                                                                                            *
 *  Module   : Cash Flow Explorer                           Component : Web Api                               *
 *  Assembly : Empiria.CashFlow.WebApi.dll                  Pattern   : Query Web Api Controller              *
-*  Type     : CashFlowAccountsTotalsController             License   : Please read LICENSE.txt file          *
+*  Type     : SearchServicesController                     License   : Please read LICENSE.txt file          *
 *                                                                                                            *
-*  Summary  : Query web API used to retrive cash flow related accounts with their totals.                    *
+*  Summary  : Query web API used to provide search services for cash flow related data and entities.         *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
 using System.Threading.Tasks;
 using System.Web.Http;
 
+using Empiria.DynamicData;
 using Empiria.WebApi;
 
 using Empiria.Financial.Adapters;
@@ -19,14 +20,14 @@ using Empiria.CashFlow.Explorer.UseCases;
 
 namespace Empiria.CashFlow.Explorer.WebApi {
 
-  /// <summary>Query web API used to retrive cash flow related accounts with their totals.</summary>
-  public class CashFlowAccountsTotalsController : WebApiController {
+  /// <summary>Query web API used to provide search services for cash flow related data and entities.</summary>
+  public class SearchServicesController : WebApiController {
 
     #region Web apis
 
     [HttpPost]
     [Route("v1/cash-flow/accounts-totals/search")]
-    public async Task<SingleObjectModel> SearchAccountsTotals([FromBody] AccountsTotalsQuery query) {
+    public async Task<SingleObjectModel> SearchAccountsTotals([FromBody] RecordsSearchQuery query) {
 
       using (var usecases = CashFlowAccountsTotalsUseCases.UseCaseInteractor()) {
         var accountsTotals = await usecases.AccountTotals(query);
@@ -35,8 +36,22 @@ namespace Empiria.CashFlow.Explorer.WebApi {
       }
     }
 
+
+    [HttpPost]
+    [Route("v1/financial-management/search-services/CreditEntries")]
+    public SingleObjectModel SearchExternalCreditEntries([FromBody] RecordsSearchQuery query) {
+
+      query.QueryType = RecordSearchQueryType.CreditEntries;
+
+      using (var services = CashFlowSearchServices.ServiceInteractor()) {
+        DynamicDto<ICreditEntryData> entries = services.SearchExternalCreditEntries(query);
+
+        return new SingleObjectModel(base.Request, entries);
+      }
+    }
+
     #endregion Web apis
 
-  }  // class CashFlowAccountsTotalsController
+  }  // class SearchServicesController
 
 }  // namespace Empiria.CashFlow.Explorer.WebApi
