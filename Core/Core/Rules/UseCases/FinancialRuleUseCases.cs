d20bd0bb51ecad4b72bee3bf743158a44f2dba75
José Manuel Cota <jmcota@ontica.org>
2025-10-23 10:49:54 -0400

    GetFinancialRuleCategories() use case and web api added

---
+++ Core/Core/Rules/UseCases/FinancialRuleUseCases.cs
@@ -0,0 +1,41 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Rules                              Component : Use Cases Layer                       *
+*  Assembly : Empiria.Financial.Core.dll                   Pattern   : Use cases                             *
+*  Type     : FinancialRuleUseCases                        License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Provides use cases for update and retrieve financial rules.                                    *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+namespace Empiria.Financial.Rules.UseCases {
+
+  /// <summary>Provides use cases for update and retrieve financial rules.</summary>
+  public class FinancialRuleUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected FinancialRuleUseCases() {
+      // no-op
+    }
+
+    static public FinancialRuleUseCases UseCaseInteractor() {
+      return UseCase.CreateInstance<FinancialRuleUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public FixedList<NamedEntityDto> GetCategories() {
+      return FinancialRuleCategory.GetList()
+                                  .MapToNamedEntityList();
+    }
+
+    #endregion Use cases
+
+  }  // class FinancialRuleUseCases
+
+}  // namespace Empiria.Financial.Rules.UseCases
---
+++ Core/WebApi/Rules/FinancialRuleController.cs
@@ -0,0 +1,40 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Financial Rules                              Component : Web Api                               *
+*  Assembly : Empiria.Financial.WebApi.dll                 Pattern   : Web api Controller                    *
+*  Type     : FinancialRuleController                      License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Web API used to retrive and update financial rules.                                            *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Web.Http;
+
+using Empiria.WebApi;
+
+using Empiria.Financial.Rules.UseCases;
+
+namespace Empiria.Financial.Rules.WebApi {
+
+  /// <summary>Web API used to retrive and update financial rules.</summary>
+  public class FinancialRuleController : WebApiController {
+
+    #region Query web apis
+
+    [HttpGet]
+    [Route("v3/financial-rules/types")]
+    public CollectionModel GetFinancialRuleCategories() {
+
+      using (var usecases = FinancialRuleUseCases.UseCaseInteractor()) {
+
+        FixedList<NamedEntityDto> categories = usecases.GetCategories();
+
+        return new CollectionModel(base.Request, categories);
+      }
+    }
+
+    #endregion Query web apis
+
+  }  // class FinancialRuleController
+
+}  // namespace Empiria.Financial.Rules.WebApi
