4d74beac65ba216b4627cb1c5409c1dfa480b1b5
José Manuel Cota <jmcota@ontica.org>
2025-11-06 08:41:04 -0500

    Rfx Separate specific report builders from CashFlowExplorer

--- CashFlow/Explorer/Domain/CashFlowExplorer.cs
+++ CashFlow/Explorer/Domain/CashFlowExplorer.cs
@@ -1,16 +1,14 @@
 ﻿/* Empiria Financial *****************************************************************************************
 *                                                                                                            *
-*  Module   : CashFlow Explorer                          Component : Use cases Layer                         *
-*  Assembly : Empiria.CashFlow.Explorer.dll              Pattern   : Use case interactor class               *
-*  Type     : CashFlowExplorerUseCases                   License   : Please read LICENSE.txt file            *
+*  Module   : CashFlow Explorer                          Component : Domain Layer                            *
+*  Assembly : Empiria.CashFlow.Explorer.dll              Pattern   : Service provider                        *
+*  Type     : CashFlowExplorer                           License   : Please read LICENSE.txt file            *
 *                                                                                                            *
-*  Summary  : Use cases used to retrieve cash flow explorer information.                                     *
+*  Summary  : Process cash account totals and return them filtered and with complete information.            *
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
 using System.Collections.Generic;
-
-using Empiria.DynamicData;
 
 using Empiria.CashFlow.CashLedger.Adapters;
 
@@ -18,6 +16,7 @@ using Empiria.CashFlow.Explorer.Adapters;
 
 namespace Empiria.CashFlow.Explorer {
 
+  /// <summary>Process cash account totals and return them filtered and with complete information.</summary>
   internal class CashFlowExplorer {
 
     private CashFlowExplorerQuery _query;
@@ -33,56 +32,30 @@ namespace Empiria.CashFlow.Explorer {
 
     #region Methods
 
-    internal DynamicDto<CashFlowExplorerEntry> Execute() {
-      return new DynamicDto<CashFlowExplorerEntry>(
-        _query,
-        GetDynamicColumns(),
-        ProcessEntries()
-      );
+    internal FixedList<CashFlowExplorerEntry> GetEntries() {
+      var entries = ProcessEntries();
+
+      entries = ApplyFilters(entries);
+
+      entries = SortEntries(entries);
+
+      return entries.ToFixedList();
     }
 
 
-    private FixedList<DataTableColumn> GetDynamicColumns() {
-      var columns = new List<DataTableColumn> {
-        new DataTableColumn("cashFlowAcct_01", "Clasificación 1", "text"),
-        new DataTableColumn("cashFlowAcct_02", "Clasificación 2", "text"),
-        new DataTableColumn("cashFlowAcct_03", "Clasificación 3", "text"),
-        new DataTableColumn("cashFlowAcct_04", "Clasificación 4", "text"),
-        new DataTableColumn("cashFlowAcct_05", "Clasificación 5", "text"),
-        new DataTableColumn("cashFlowAcct_06", "Clasificación 6", "text"),
-        new DataTableColumn("program", "Programa", "text"),
-        new DataTableColumn("subprogram", "Subprograma", "text"),
-        new DataTableColumn("financingSource", "Fuente", "text"),
-        new DataTableColumn("operationType", "Operación", "text"),
-        new DataTableColumn("cashAccountNo", "Concepto", "text"),
-        new DataTableColumn("conceptDescription", "Descripción", "text"),
-        new DataTableColumn("organizationalUnit", "Área", "text"),
-        new DataTableColumn("currencyCode", "Moneda", "text"),
-        new DataTableColumn("inflows", "Entradas", "decimal"),
-        new DataTableColumn("outflows", "Salidas", "decimal"),
-      };
-
-      return columns.ToFixedList();
-    }
-
-
-    private FixedList<CashFlowExplorerEntry> ProcessEntries() {
+    private List<CashFlowExplorerEntry> ProcessEntries() {
       var entries = new List<CashFlowExplorerEntry>(_totals.Count);
 
       foreach (var entry in _totals) {
         entries.Add(ProcessTotal(entry));
       }
 
-      var filteredEntries = ApplyFilters(entries);
-
-      return filteredEntries.Sort(x => $"{x.StandardAccountNo.PadRight(64)}|{x.OperationType.PadRight(64)}|{x.CashAccountNo.PadRight(16)}|" +
-                                      $"{x.ConceptDescription.PadRight(300)}|{x.OrganizationalUnit.PadRight(255)}||{x.CurrencyCode}");
+      return entries;
     }
 
 
-    private FixedList<CashFlowExplorerEntry> ApplyFilters(List<CashFlowExplorerEntry> entries) {
-      var filtered = entries.ToFixedList()
-                            .FindAll(x => x.CashAccountId > 0);
+    private List<CashFlowExplorerEntry> ApplyFilters(List<CashFlowExplorerEntry> entries) {
+      var filtered = entries.FindAll(x => x.CashAccountId > 0);
 
       if (_query.OperationTypeUID.Length != 0) {
         filtered = filtered.FindAll(x => x.CashAccount.OperationType.UID == _query.OperationTypeUID);
@@ -98,7 +71,7 @@ namespace Empiria.CashFlow.Explorer {
         filtered = filtered.FindAll(x => EmpiriaString.ContainsAny(x.CashAccount.Parent.Keywords, queryKeywords));
       }
 
-      return filtered.ToFixedList();
+      return filtered;
     }
 
 
@@ -112,6 +85,14 @@ namespace Empiria.CashFlow.Explorer {
       entry.Sum(total);
 
       return entry;
+    }
+
+
+    private List<CashFlowExplorerEntry> SortEntries(List<CashFlowExplorerEntry> entries) {
+      return new List<CashFlowExplorerEntry>(
+                    entries.ToFixedList()
+                    .Sort(x => $"{x.StandardAccountNo.PadRight(64)}|{x.OperationType.PadRight(64)}|{x.CashAccountNo.PadRight(16)}|" +
+                               $"{x.ConceptDescription.PadRight(300)}|{x.OrganizationalUnit.PadRight(255)}||{x.CurrencyCode}"));
     }
 
     #endregion Methods
---
+++ CashFlow/Explorer/Domain/CashFlowTotalsReport.cs
@@ -0,0 +1,51 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : CashFlow Explorer                          Component : Domain Layer                            *
+*  Assembly : Empiria.CashFlow.Explorer.dll              Pattern   : Report Builder                          *
+*  Type     : CashFlowTotalsReport                      License   : Please read LICENSE.txt file             *
+*                                                                                                            *
+*  Summary  : Builds a cash flow totals report according to a cash flow accounts tree.                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Collections.Generic;
+
+using Empiria.DynamicData;
+
+using Empiria.CashFlow.Explorer.Adapters;
+
+namespace Empiria.CashFlow.Explorer.UseCases {
+
+  /// <summary>Builds a cash flow totals report according to a cash flow accounts tree.</summary>
+  internal class CashFlowTotalsReport {
+
+    private CashFlowExplorerQuery _query;
+    private FixedList<CashFlowExplorerEntry> _sourceEntries;
+
+    public CashFlowTotalsReport(CashFlowExplorerQuery query,
+                                FixedList<CashFlowExplorerEntry> sourceEntries) {
+      _query = query;
+      _sourceEntries = sourceEntries;
+    }
+
+    internal void Build() {
+
+    }
+
+    internal DynamicDto<CashFlowExplorerEntry> ToDynamicDto() {
+      return new DynamicDto<CashFlowExplorerEntry>(_query, GetColumns(), _sourceEntries);
+    }
+
+    private FixedList<DataTableColumn> GetColumns() {
+      return new List<DataTableColumn> {
+        new DataTableColumn("account", "Cuenta", "text"),
+        new DataTableColumn("subprogram", "Descripción", "text"),
+        new DataTableColumn("currencyCode", "Moneda", "text"),
+        new DataTableColumn("inflows", "Entradas", "decimal"),
+        new DataTableColumn("outflows", "Salidas", "decimal"),
+      }.ToFixedList();
+    }
+
+  }  // class CashFlowTotalsReport
+
+}  // namespace Empiria.CashFlow.Explorer.UseCases
