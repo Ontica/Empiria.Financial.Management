33a01d3da73afed13a8a7b119c9d7257aae57a55
José Manuel Cota <jmcota@ontica.org>
2025-12-03 23:27:56 -0500

    Rfx IPayableEntity was moved from Empiria.Central to Payments.Core

---
+++ Core/Core/Payments/IPayableEntity.cs
@@ -0,0 +1,113 @@
+﻿/* Empiria Central  ******************************************************************************************
+*                                                                                                            *
+*  Module   : Financial                                  Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Integration interface                   *
+*  Type     : IPayableEntity                             License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Defines a payable entity. Payable entities are purchase orders,                                *
+*             contract supply orders, invoices, paychecks, etc.                                              *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Ontology;
+
+namespace Empiria.Financial {
+
+  /// <summary>Defines a payable entity. Payable entities are purchase orders,
+  /// contract supply orders, invoices, paychecks, etc.</summary>
+  public interface IPayableEntity : INamedEntity {
+
+    int Id {
+      get;
+    }
+
+    ObjectTypeInfo GetEmpiriaType();
+
+
+    string EntityNo {
+      get;
+    }
+
+
+    string Description {
+      get;
+    }
+
+    INamedEntity OrganizationalUnit {
+      get;
+    }
+
+    INamedEntity PayTo {
+      get;
+    }
+
+    INamedEntity Currency {
+      get;
+    }
+
+    decimal Total {
+      get;
+    }
+
+    INamedEntity Budget {
+      get;
+    }
+
+    INamedEntity Project {
+      get;
+    }
+
+    FixedList<IPayableEntityItem> Items {
+      get;
+    }
+
+  }  // interface IPayableEntity
+
+
+  /// <summary>Defines a payable entity item.</summary>
+  public interface IPayableEntityItem {
+
+    int Id {
+      get;
+    }
+
+    string UID {
+      get;
+    }
+
+    decimal Quantity {
+      get;
+    }
+
+    INamedEntity Unit {
+      get;
+    }
+
+    decimal UnitPrice {
+      get;
+    }
+
+    INamedEntity Currency {
+      get;
+    }
+
+    INamedEntity Product {
+      get;
+    }
+
+    string Description {
+      get;
+    }
+
+    INamedEntity BudgetAccount {
+      get;
+    }
+
+
+    decimal Subtotal {
+      get;
+    }
+
+  }  // interface IPayableEntityItem
+
+}  // namespace Empiria.Financial
