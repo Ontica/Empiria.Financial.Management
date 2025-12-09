d71fe6d86f3d8a39570102f9412c23d3b02a8425
José Manuel Cota <jmcota@ontica.org>
2025-12-08 23:19:33 -0500

    BudgetType.StandardAccountType property added

--- Budgeting/Core/Budgets/Domain/BudgetType.cs
+++ Budgeting/Core/Budgets/Domain/BudgetType.cs
@@ -72,6 +72,12 @@ namespace Empiria.Budgeting {
       }
     }
 
+    public StandardAccountType StandardAccountType {
+      get {
+        return base.ExtensionData.Get("standardAccountTypeId", StandardAccountType.Empty);
+      }
+    }
+
     #endregion Properties
 
     #region Methods
