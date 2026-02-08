d11293fb022abff92cf1a40c49227ed18071b022
José Manuel Cota <jmcota@ontica.org>
2026-02-08 13:29:10 -0500

    Rfx PaymentAccounts.Payee was moved from Procurement.Suppliers module

---
+++ Payments/Core/PaymentAccounts/Domain/Payee.cs
@@ -0,0 +1,150 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Payments Management                          Component : Domain Layer                          *
+*  Assembly : Empiria.Payments.Core.dll                    Pattern   : Information Holder                    *
+*  Type     : Payee                                        License   : Please read LICENSE.txt file          *
+*                                                                                                            *
+*  Summary  : Represents a payee. A payee is someone who receives payments.                                  *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System;
+
+using Empiria.Parties;
+using Empiria.StateEnums;
+
+using Empiria.Payments.Data;
+
+namespace Empiria.Payments {
+
+  /// <summary>Represents a payee. A payee is someone who receives payments.</summary>
+  public class Payee : Party, INamedEntity {
+
+    #region Constructors and parsers
+
+    protected Payee(PartyType partyType) : base(partyType) {
+      // Required by Empiria Framework.
+    }
+
+    internal Payee(PayeeFields fields) {
+      Assertion.Require(fields, nameof(fields));
+
+      fields.EnsureValid();
+
+      Update(fields);
+    }
+
+    static public new Payee Parse(int id) => ParseId<Payee>(id);
+
+    static public new Payee Parse(string uid) => ParseKey<Payee>(uid);
+
+    static public new Payee Empty => ParseEmpty<Payee>();
+
+    #endregion Constructors and parsers
+
+    #region Properties
+
+    public PayeeType PayeeType {
+      get {
+        return PayeeType.Parse(ExtendedData.Get("supplierType", PayeeType.Unknown.Name));
+      }
+      private set {
+        ExtendedData.SetIfValue("supplierType", value.Name);
+      }
+    }
+
+
+    public string EmployeeNo {
+      get {
+        return ExtendedData.Get("employeeNo", string.Empty);
+      }
+      private set {
+        ExtendedData.SetIfValue("employeeNo", value);
+      }
+    }
+
+
+    public string SubledgerAccount {
+      get {
+        return ExtendedData.Get("taxData/subledgerAccount", string.Empty);
+      }
+      private set {
+        ExtendedData.SetIfValue("taxData/subledgerAccount", value);
+      }
+    }
+
+
+    public string SubledgerAccountName {
+      get {
+        return ExtendedData.Get("taxData/subledgerAccountName", string.Empty);
+      }
+      private set {
+        ExtendedData.SetIfValue("taxData/subledgerAccountName", value);
+      }
+    }
+
+
+    public string TaxCode {
+      get {
+        return Code;
+      }
+      private set {
+        Code = value;
+      }
+    }
+
+    string INamedEntity.Name {
+      get {
+        if (EmployeeNo.Length != 0) {
+          return $"{Name} (No. expediente: {EmployeeNo})";
+        }
+        return $"{Name} ({TaxCode})";
+      }
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    internal void Delete() {
+      SetStatus(EntityStatus.Deleted);
+    }
+
+
+    protected override void OnBeforeSave() {
+      if (IsNew) {
+        PatchObjectId(PayeesData.GetNextPayeeId());
+      }
+    }
+
+
+    internal void Update(PayeeFields fields) {
+      Assertion.Require(fields, nameof(fields));
+
+      PayeeType = PayeeType.Parse(fields.TypeUID);
+      TaxCode = Patcher.Patch(fields.TaxCode, TaxCode);
+
+      SubledgerAccount = Patcher.Patch(fields.SubledgerAccount, SubledgerAccount);
+      SubledgerAccountName = Patcher.Patch(fields.SubledgerAccountName, SubledgerAccountName);
+
+      EmployeeNo = EmpiriaString.Clean(fields.EmployeeNo);
+
+      if (fields.UID.Length == 0) {
+        fields.StartDate = DateTime.Today;
+      } else {
+        fields.StartDate = StartDate;
+      }
+
+      fields.EndDate = ExecutionServer.DateMaxValue;
+
+      fields.Roles = EmpiriaString.Tagging("supplier")
+                                  .ToArray();
+
+      base.Update(fields);
+    }
+
+    #endregion Methods
+
+  } // class Payee
+
+} // namespace Empiria.Payments
---
+++ Payments/Core/PaymentAccounts/UseCases/PaymentAccountsUseCases.cs
@@ -0,0 +1,90 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Payments Management                        Component : Use cases Layer                         *
+*  Assembly : Empiria.Payments.Core.dll                  Pattern   : Use case interactor class               *
+*  Type     : PaymentAccountsUseCases                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Use cases for payments accounts.                                                               *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using Empiria.Services;
+
+using Empiria.Financial;
+using Empiria.Financial.Adapters;
+using Empiria.Financial.Services;
+
+namespace Empiria.Payments.UseCases {
+
+  /// <summary>Use cases for payments accounts.</summary>
+  public class PaymentAccountsUseCases : UseCase {
+
+    #region Constructors and parsers
+
+    protected PaymentAccountsUseCases() {
+
+    }
+
+    static public PaymentAccountsUseCases UseCaseInteractor() {
+      return CreateInstance<PaymentAccountsUseCases>();
+    }
+
+    #endregion Constructors and parsers
+
+    #region Use cases
+
+    public PaymentAccountDto AddPaymentAccount(Payee payee, PaymentAccountFields fields) {
+      Assertion.Require(payee, nameof(payee));
+      Assertion.Require(fields, nameof(fields));
+
+      var account = new PaymentAccount(payee, fields);
+
+      account.Save();
+
+      return new PaymentAccountDto(account);
+    }
+
+
+    public FixedList<PaymentAccountDto> GetPaymentAccounts(string payeeUID) {
+      Assertion.Require(payeeUID, nameof(payeeUID));
+
+      var payee = Payee.Parse(payeeUID);
+
+      return PaymentAccountServices.GetPaymentAccounts(payee);
+    }
+
+
+    public PaymentAccountDto RemovePaymentAccount(Payee payee, PaymentAccount account) {
+      Assertion.Require(payee, nameof(payee));
+      Assertion.Require(account, nameof(account));
+
+      Assertion.Require(account.Party.Equals(payee), "La cuenta no pertenece al beneficiario especificado.");
+
+      account.Delete();
+
+      account.Save();
+
+      return new PaymentAccountDto(account);
+    }
+
+
+    public PaymentAccountDto UpdatePaymentAccount(Payee payee, PaymentAccountFields fields) {
+      Assertion.Require(payee, nameof(payee));
+      Assertion.Require(fields, nameof(fields));
+
+      var account = PaymentAccount.Parse(fields.UID);
+
+      Assertion.Require(account.Party.Equals(payee), "La cuenta no pertenece al beneficiario especificado.");
+
+      account.Update(fields);
+
+      account.Save();
+
+      return new PaymentAccountDto(account);
+    }
+
+    #endregion Use cases
+
+  }  // class PaymentAccountsUseCases
+
+}  // namespace Empiria.Payments.UseCases
