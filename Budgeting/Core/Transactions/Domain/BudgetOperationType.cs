67b1678a3a17dfdf4c0de73725d6d5b369e57521
José Manuel Cota <jmcota@ontica.org>
2026-02-20 18:59:41 -0500

    BudgetOperationType.DepositColumn()/DefaultWithdrawalColumn() added

--- Budgeting/Core/Transactions/Domain/BudgetOperationType.cs
+++ Budgeting/Core/Transactions/Domain/BudgetOperationType.cs
@@ -19,6 +19,8 @@ namespace Empiria.Budgeting.Transactions {
 
     Expand,
 
+    Reduce,
+
     Modify,
 
     Request,
@@ -32,5 +34,85 @@ namespace Empiria.Budgeting.Transactions {
     None
 
   }  // enum BudgetOperationType
+
+
+
+  /// <summary>Extension methods for BudgetOperationType enumeration.</summary>
+  public static class BudgetOperationTypeExtensionMethods {
+
+    static public BalanceColumn DepositColumn(this BudgetOperationType operationType) {
+
+      switch (operationType) {
+
+        case BudgetOperationType.Plan:
+          return BalanceColumn.Planned;
+
+        case BudgetOperationType.Authorize:
+          return BalanceColumn.Authorized;
+
+        case BudgetOperationType.Expand:
+          return BalanceColumn.Expanded;
+
+        case BudgetOperationType.Reduce:
+          return BalanceColumn.Reduced;
+
+        case BudgetOperationType.Request:
+          return BalanceColumn.Requested;
+
+        case BudgetOperationType.Commit:
+          return BalanceColumn.Commited;
+
+        case BudgetOperationType.ApprovePayment:
+          return BalanceColumn.ToPay;
+
+        case BudgetOperationType.Exercise:
+          return BalanceColumn.Exercised;
+
+        case BudgetOperationType.None:
+          return BalanceColumn.Empty;
+
+        default:
+          throw Assertion.EnsureNoReachThisCode($"Unhandled budget operation type '{operationType}'.");
+      }
+    }
+
+
+    static public BalanceColumn DefaultWithdrawalColumn(this BudgetOperationType operationType) {
+
+      switch (operationType) {
+
+        case BudgetOperationType.Plan:
+          return BalanceColumn.Empty;
+
+        case BudgetOperationType.Authorize:
+          return BalanceColumn.Empty;
+
+        case BudgetOperationType.Expand:
+          return BalanceColumn.Expanded;
+
+        case BudgetOperationType.Reduce:
+          return BalanceColumn.Reduced;
+
+        case BudgetOperationType.Request:
+          return BalanceColumn.Available;
+
+        case BudgetOperationType.Commit:
+          return BalanceColumn.Requested;
+
+        case BudgetOperationType.ApprovePayment:
+          return BalanceColumn.Commited;
+
+        case BudgetOperationType.Exercise:
+          return BalanceColumn.ToPay;
+
+        case BudgetOperationType.None:
+          return BalanceColumn.Empty;
+
+        default:
+          throw Assertion.EnsureNoReachThisCode($"Unhandled budget operation type '{operationType}'.");
+      }
+    }
+
+  }  // class BudgetOperationTypeExtensionMethods
 
 }  // namespace Empiria.Budgeting.Transactions
