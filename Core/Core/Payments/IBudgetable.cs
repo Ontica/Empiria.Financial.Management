1cf799145a887b4151affac6180416602193bcea
José Manuel Cota <jmcota@ontica.org>
2026-02-21 22:46:46 -0500

    FixedList<BudgetableItemData> IBudgetable.Items property added

--- Core/Core/Payments/IBudgetable.cs
+++ Core/Core/Payments/IBudgetable.cs
@@ -10,7 +10,13 @@
 *                                                                                                            *
 ************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
 
+using System;
+
+using Empiria.Locations;
 using Empiria.Ontology;
+using Empiria.Parties;
+using Empiria.Products;
+using Empiria.Projects;
 
 namespace Empiria.Financial {
 
@@ -20,6 +26,10 @@ namespace Empiria.Financial {
   public interface IBudgetable : IIdentifiable, INamedEntity {
 
     string EntityNo {
+      get;
+    }
+
+    FixedList<BudgetableItemData> Items {
       get;
     }
 
@@ -33,6 +43,142 @@ namespace Empiria.Financial {
 
   } // interface IBudgetable
 
+
+  /// <summary>
+  /// Holds data for budgetable items. A budgetable item represents a single line item in a budgetable entity, 
+  /// such as a product or service in an order or requisition, or a payment liability in a loan or payable order.
+  /// </summary>
+  public class BudgetableItemData {
+
+    public IIdentifiable BudgetableItem {
+      get; set;
+    }
+
+    public IIdentifiable Budget {
+      get; set;
+    }
+
+    public FinancialAccount BudgetAccount {
+      get; set;
+    } = FinancialAccount.Empty;
+
+
+    public Product Product {
+      get; set;
+    } = Product.Empty;
+
+
+    public string ProductCode {
+      get; set;
+    } = string.Empty;
+
+
+    public string ProductName {
+      get; set;
+    } = string.Empty;
+
+
+    public string Description {
+      get; set;
+    } = string.Empty;
+
+
+    public string Justification {
+      get; set;
+    } = string.Empty;
+
+
+    public ProductUnit ProductUnit {
+      get; set;
+    } = ProductUnit.Empty;
+
+
+    public Country OriginCountry {
+      get; set;
+    } = Country.Default;
+
+
+    public decimal ProductQty {
+      get; set;
+    }
+
+    public Project Project {
+      get; set;
+    } = Project.Empty;
+
+
+    public Party Party {
+      get; set;
+    } = Party.Empty;
+
+
+    public DateTime BudgetingDate {
+      get; set;
+    } = DateTime.Today.Date;
+
+
+    public Currency Currency {
+      get; set;
+    } = Currency.Default;
+
+
+    public decimal ExchangeRate {
+      get; set;
+    } = decimal.One;
+
+
+    public decimal CurrencyAmount {
+      get; set;
+    }
+
+
+    public decimal Amount {
+      get {
+        return Math.Round(CurrencyAmount * ExchangeRate, 2);
+      }
+    }
+
+
+    public bool HasRelatedBudgetEntry {
+      get {
+        return RelatedBudgetEntry != null;
+      }
+    }
+
+    public IIdentifiable RelatedBudgetEntry {
+      get; set;
+    }
+
+    public void EnsureValid() {
+      Assertion.Require(BudgetableItem, nameof(BudgetableItem));
+      Assertion.Require(Budget, nameof(Budget));
+      Assertion.Require(BudgetAccount != FinancialAccount.Empty, nameof(BudgetAccount));
+      Assertion.Require(Product, nameof(Product));
+      Assertion.Require(ProductCode != null, nameof(ProductCode));
+      Assertion.Require(ProductName != null, nameof(ProductName));
+      Assertion.Require(ProductUnit, nameof(ProductUnit));
+      Assertion.Require(OriginCountry, nameof(OriginCountry));
+      Assertion.Require(Description != null, nameof(Description));
+      Assertion.Require(Justification != null, nameof(Justification));
+      Assertion.Require(ProductQty > 0, $"{nameof(ProductQty)} must be greater than zero.");
+      Assertion.Require(Project, nameof(Project));
+      Assertion.Require(Party, nameof(Party));
+      Assertion.Require(!ExecutionServer.IsMinOrMaxDate(BudgetingDate), $"Invalid {nameof(BudgetingDate)}.");
+      Assertion.Require(BudgetingDate.Year >= DateTime.Today.Year, $"Invalid {nameof(BudgetingDate)}.");
+      Assertion.Require(Currency, nameof(Currency));
+      Assertion.Require(ExchangeRate > decimal.Zero, $"{nameof(ExchangeRate)} must be positive.");
+      Assertion.Require(CurrencyAmount > decimal.Zero, $"{nameof(CurrencyAmount)} must be positive.");
+
+      if (Currency.Equals(Currency.Default)) {
+        Assertion.Require(ExchangeRate == decimal.One,
+          $"{nameof(ExchangeRate)} must be equal to $1.00 when currency is default.");
+      } else {
+        Assertion.Require(ExchangeRate != decimal.One,
+          $"{nameof(ExchangeRate)} must be distinct to $1.00 when currency is not default.");
+      }
+    }
+
+  }  // class BudgetableItemData
 
 
   /// <summary>Represents a tax entry for budgetable entities.</summary>
