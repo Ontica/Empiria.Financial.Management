48589f7d31f1416d039a2572b2d73bd67f30bd60
José Manuel Cota <jmcota@ontica.org>
2026-03-18 22:39:46 -0400

    BudgetTransactionBalancer new rules added including global budget account

--- Budgeting/Core/Transactions/UseCases/BudgetTransactionBalancer.cs
+++ Budgeting/Core/Transactions/UseCases/BudgetTransactionBalancer.cs
@@ -11,6 +11,8 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Empiria.Parties;
+
 using Empiria.Budgeting.Explorer;
 using Empiria.Budgeting.Explorer.Adapters;
 
@@ -57,19 +59,17 @@ namespace Empiria.Budgeting.Transactions {
         var needed = deposit.Sum(x => x.Deposit) - withdrawalAmount;
 
         if (needed == 0) {
-          continue;
+          goto NextDeposit;
         }
 
-        var available = _availableBudget.FindAll(x => x.BudgetAccount.Equals(account) &&
-                                                      x.Month == month)
-                                        .Sum(x => x.Available);
+        var available = GetAvailableBudget(account, month);
 
         if (available >= needed) {
           BudgetEntry entry = BuildEntry(account, BalanceColumn.Available, month, -needed);
 
           entries.Add(entry);
 
-          continue;
+          goto NextDeposit;
 
         } else if (available > 0) {
           BudgetEntry entry = BuildEntry(account, BalanceColumn.Available, month, -available);
@@ -79,46 +79,162 @@ namespace Empiria.Budgeting.Transactions {
           needed = needed - available;
         }
 
-        for (int monthIndex = 12; monthIndex > month; monthIndex--) {
-          available = _availableBudget.FindAll(x => x.BudgetAccount.Equals(account) &&
-                                                    x.Month == monthIndex)
-                                      .Sum(x => x.Available);
+        if (needed <= 0) {
+          goto NextDeposit;
+        }
+
+        for (int monthIndex = 1; monthIndex < month; monthIndex++) {
+
+          available = GetAvailableBudget(account, monthIndex);
 
           if (available >= needed) {
 
-            BudgetEntry entry = BuildEntry(account, BalanceColumn.Reduced, monthIndex, needed);
+            var reduceExpand = BuildReduceExpandEntries(account, monthIndex, account, month, needed);
 
-            entries.Add(entry);
+            entries.AddRange(reduceExpand);
 
-            var entry2 = BuildEntry(account, BalanceColumn.Expanded, month, needed);
-
-            entries.Add(entry2);
-
-            break;
+            goto NextDeposit;
 
           } else if (available > 0) {
 
-            BudgetEntry entry = BuildEntry(account, BalanceColumn.Reduced, monthIndex, available);
+            var reduceExpand = BuildReduceExpandEntries(account, monthIndex, account, month, available);
 
-            entries.Add(entry);
-
-            var entry2 = BuildEntry(account, BalanceColumn.Expanded, month, available);
-
-            entries.Add(entry2);
+            entries.AddRange(reduceExpand);
 
             needed = needed - available;
           }
         }
 
+        if (needed <= 0) {
+          goto NextDeposit;
+        }
+
+        for (int monthIndex = 12; monthIndex > month; monthIndex--) {
+
+          available = GetAvailableBudget(account, monthIndex);
+
+          if (available >= needed) {
+
+            var reduceExpand = BuildReduceExpandEntries(account, monthIndex, account, month, needed);
+
+            entries.AddRange(reduceExpand);
+
+            goto NextDeposit;
+
+          } else if (available > 0) {
+
+            var reduceExpand = BuildReduceExpandEntries(account, monthIndex, account, month, available);
+
+            entries.AddRange(reduceExpand);
+
+            needed = needed - available;
+          }
+        }
+
+        if (needed <= 0) {
+          goto NextDeposit;
+        }
+
+        var globalAcount = TryGetGlobalBudgetAccount(account);
+
+        if (globalAcount == null) {
+          goto NextDeposit;
+        }
+
+        available = GetAvailableBudget(globalAcount, month);
+
+        if (available >= needed) {
+
+          var reduceExpand = BuildReduceExpandEntries(globalAcount, month, account, month, needed);
+
+          entries.AddRange(reduceExpand);
+
+          goto NextDeposit;
+
+        } else if (available > 0) {
+
+          var reduceExpand = BuildReduceExpandEntries(globalAcount, month, account, month, available);
+
+          entries.AddRange(reduceExpand);
+
+          needed = needed - available;
+        }
+
+        if (needed <= 0) {
+          goto NextDeposit;
+        }
+
+        for (int monthIndex = 1; monthIndex < month; monthIndex++) {
+
+          available = GetAvailableBudget(globalAcount, monthIndex);
+
+          if (available >= needed) {
+
+            var reduceExpand = BuildReduceExpandEntries(globalAcount, monthIndex, account, month, needed);
+
+            entries.AddRange(reduceExpand);
+
+            goto NextDeposit;
+
+          } else if (available > 0) {
+
+            var reduceExpand = BuildReduceExpandEntries(globalAcount, monthIndex, account, month, available);
+
+            entries.AddRange(reduceExpand);
+
+            needed = needed - available;
+          }
+        }
+
+        if (needed <= 0) {
+          goto NextDeposit;
+        }
+
+        for (int monthIndex = 12; monthIndex > month; monthIndex--) {
+
+          available = GetAvailableBudget(globalAcount, monthIndex);
+
+          if (available >= needed) {
+
+            var reduceExpand = BuildReduceExpandEntries(globalAcount, monthIndex, account, month, needed);
+
+            entries.AddRange(reduceExpand);
+
+            goto NextDeposit;
+
+          } else if (available > 0) {
+
+            var reduceExpand = BuildReduceExpandEntries(globalAcount, monthIndex, account, month, available);
+
+            entries.AddRange(reduceExpand);
+
+            needed = needed - available;
+          }
+        }
+
+NextDeposit:
+        ;
       }  // foreach
 
       return entries.ToFixedList();
     }
 
+
+    private BudgetEntry[] BuildReduceExpandEntries(BudgetAccount reduceAccount, int reduceMonth,
+                                                   BudgetAccount expandAccount, int expandMonth,
+                                                   decimal amount) {
+
+      BudgetEntry reduce = BuildEntry(reduceAccount, BalanceColumn.Reduced, reduceMonth, amount);
+
+      BudgetEntry expand = BuildEntry(expandAccount, BalanceColumn.Expanded, expandMonth, amount);
+
+      return new[] { reduce, expand };
+    }
+
     #region Helpers
 
     private BudgetEntry BuildEntry(BudgetAccount account, BalanceColumn balanceColumn,
-                                  int month, decimal amount) {
+                                   int month, decimal amount) {
 
       var entry = new BudgetEntry(_transaction, account, month,
                                   balanceColumn, amount, true);
@@ -136,6 +252,20 @@ namespace Empiria.Budgeting.Transactions {
       var available = new AvailableBudgetBuilder(query);
 
       return available.Build();
+    }
+
+
+    private decimal GetAvailableBudget(BudgetAccount account, int month) {
+      return _availableBudget.FindAll(x => x.BudgetAccount.Equals(account) &&
+                                           x.Month == month)
+                             .Sum(x => x.Available);
+    }
+
+
+    private BudgetAccount TryGetGlobalBudgetAccount(BudgetAccount account) {
+      var globalOrgUnit = OrganizationalUnit.TryParseWithID("901200");
+
+      return BudgetAccount.TryParse(globalOrgUnit, account.AccountNo);
     }
 
     #endregion Helpers
