237220e845f78b772038ca2e444773735fa89e39
José Manuel Cota <jmcota@ontica.org>
2026-08-13 11:09:28 -0400

    BudgetTransactionValidator service added [initial version]

---
+++ Budgeting/Core/Transactions/UseCases/BudgetTransactionValidator.cs
@@ -0,0 +1,84 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Budget Transactions                        Component : Services Layer                          *
+*  Assembly : Empiria.Budgeting.Core.dll                 Pattern   : Builder                                 *
+*  Type     : BudgetTransactionValidator                 License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Provides services used to validate a budget transaction.                                       *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+using System.Linq;
+
+namespace Empiria.Budgeting.Transactions {
+
+  /// <summary>Provides services used to validate a budget transaction.</summary>
+  public class BudgetTransactionValidator {
+
+    private readonly BudgetTransaction _transaction;
+    private readonly FixedList<BudgetTransaction> _relatedTransactions;
+
+    public BudgetTransactionValidator(BudgetTransaction transaction) {
+
+      Assertion.Require(transaction, nameof(transaction));
+
+      Assertion.Require(transaction.Entries.Count > 0,
+                        "Transaction has no entries.");
+
+      Assertion.Require(transaction.Entries.SelectDistinct(x => x.Year).Count == 1,
+                        "Transaction can not be multiyear.");
+
+      _transaction = transaction;
+
+      _relatedTransactions = BudgetTransaction.GetRelatedTo(transaction);
+    }
+
+
+    public void EnsureHasAvailableBudget() {
+
+      var deposits = _transaction.Entries.FindAll(x => x.Deposit > 0 && x.NotAdjustment &&
+                                                  x.BudgetAccount.StandardAccount.RoleType != Financial.AccountRoleType.Control)
+                                         .GroupBy(x => new { x.BudgetAccount, x.Month });
+
+
+      foreach (var deposit in deposits) {
+
+        BudgetAccount account = deposit.Key.BudgetAccount;
+        decimal requiredAmount = deposit.Sum(x => x.Deposit);
+
+        decimal depositsTotal = GetDepositsTotal(_transaction.OperationType.DefaultWithdrawalColumn(), account);
+
+        decimal withdrawalsTotal = GetWithdrawalsTotal(_transaction.OperationType.DefaultWithdrawalColumn(), account);
+
+        if (requiredAmount > withdrawalsTotal) {
+          Assertion.RequireFail($"No hay presupuesto comprometido disponible para la partida {account.AccountNo}: " +
+                                $"Disponible {(depositsTotal - withdrawalsTotal):C2}, Solicitado {requiredAmount:C2}");
+        }
+      }
+    }
+
+
+    #region Helpers
+
+    private decimal GetDepositsTotal(BalanceColumn balanceColumn, BudgetAccount account) {
+      return _relatedTransactions.SelectFlat(x => x.Entries)
+                                 .FindAll(x => x.BudgetAccount.Equals(account) &&
+                                               x.BalanceColumn.Equals(balanceColumn) &&
+                                               x.Deposit > 0 && !x.IsAdjustment)
+                                 .Sum(x => x.Deposit);
+    }
+
+
+    private decimal GetWithdrawalsTotal(BalanceColumn balanceColumn, BudgetAccount account) {
+      return _relatedTransactions.SelectFlat(x => x.Entries)
+                                 .FindAll(x => x.BudgetAccount.Equals(account) &&
+                                               x.BalanceColumn.Equals(balanceColumn) &&
+                                               x.Withdrawal > 0 && !x.IsAdjustment)
+                                 .Sum(x => x.Withdrawal);
+    }
+
+    #endregion Helpers
+
+  }  // class BudgetTransactionValidator
+
+}  // namespace Empiria.Budgeting.Transactions
