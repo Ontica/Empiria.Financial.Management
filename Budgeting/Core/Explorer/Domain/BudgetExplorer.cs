752e5669ca996ef0c4363b666be8aad726cd6206
José Manuel Cota <jmcota@ontica.org>
2026-08-19 20:07:51 -0400

    BudgetExplorer apply budget accounts filter

--- Budgeting/Core/Explorer/Domain/BudgetExplorer.cs
+++ Budgeting/Core/Explorer/Domain/BudgetExplorer.cs
@@ -98,12 +98,16 @@ namespace Empiria.Budgeting.Explorer {
 
 
     private FixedList<BudgetDataInColumns> FilterBudgetData(FixedList<BudgetDataInColumns> budgetData) {
-      if (_command.OrganizationalUnits.Count == 0) {
-        return budgetData;
 
-      } else {
-        return budgetData.FindAll(x => _command.OrganizationalUnits.Contains(x.BudgetAccount.OrganizationalUnit));
+      if (_command.OrganizationalUnits.Count != 0) {
+        budgetData = budgetData.FindAll(x => _command.OrganizationalUnits.Contains(x.BudgetAccount.OrganizationalUnit));
       }
+
+      if (_command.BudgetAccounts.Length != 0) {
+        budgetData = budgetData.FindAll(x => _command.BudgetAccounts.Any(y => x.BudgetAccount.Code.StartsWith(y)));
+      }
+
+      return budgetData;
     }
 
 
